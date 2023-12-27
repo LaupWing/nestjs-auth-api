6ccabb13c5aefc7b65424aaac9905650e17c2f49
LaupWing <loc_strict@hotmail.com>
2023-12-27 18:24:14 +0100

    added some prisma stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { UsersModule } from "./users/users.module"
 import { ApisModule } from './apis/apis.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-   imports: [UsersModule, ApisModule],
+   imports: [UsersModule, ApisModule, PrismaModule],
    controllers: [AppController],
    providers: [AppService],
 })
