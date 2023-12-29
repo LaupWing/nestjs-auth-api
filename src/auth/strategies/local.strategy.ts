e0b9a274bb4d5a589a6c3215f8180f0f71e8e449
LaupWing <loc_strict@hotmail.com>
2023-12-29 20:47:12 +0100

    generated base

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,16 @@
+import { Strategy } from "passport-local"
+import { PassportStrategy } from "@nestjs/passport"
+import { Injectable } from "@nestjs/common"
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+   constructor() {
+      super()
+   }
+
+   async validate(username: string, password: string): Promise<any> {
+      // Add your validation logic here
+      // For example, check if the username and password are valid
+      // and return the user object if they are valid
+   }
+}
