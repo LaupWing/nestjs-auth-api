e9a1cde2458d93f47aded1d708cc23f1a620ecbb
LaupWing <loc_strict@hotmail.com>
2023-12-29 20:50:15 +0100

    updated

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -5,10 +5,12 @@ import { Injectable } from "@nestjs/common"
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
    constructor() {
-      super()
+      super({
+         usernameField: "email"
+      })
    }
 
-   async validate(username: string, password: string): Promise<any> {
+   async validate(email: string, password: string): Promise<any> {
       // Add your validation logic here
       // For example, check if the username and password are valid
       // and return the user object if they are valid
